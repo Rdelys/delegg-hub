ff921917fd9efad1af31bf6f19b7198b53de4e4f
Rdelys <rdauphinelys@gmail.com>
2026-03-11 16:38:41 +0300

    menu

---
+++ resources/views/client/invoice/flux.blade.php
@@ -0,0 +1,12 @@
+@extends('client.layouts.app')
+
+@section('title', 'Invoice - Flux Auto')
+
+@section('content')
+
+<div class="card">
+    <h2>Flux Automatique</h2>
+    <p>Prochainement...</p>
+</div>
+
+@endsection
